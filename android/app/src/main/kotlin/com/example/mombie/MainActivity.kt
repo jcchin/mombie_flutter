d166602ce53c1736f69243d7a0cdb3b8a757b24b
jcchin <jchin89@gmail.com>
2020-11-11 01:13:41 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/mombie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mombie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/signup.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:mombie/home.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+
+import 'email_login.dart';
+import 'email_signup.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
+final FirebaseAuth _auth = FirebaseAuth.instance;
+DatabaseReference dbRef =
+FirebaseDatabase.instance.reference().child("Users");
+
+
+class SignUp extends StatelessWidget {
+  final String title = "Sign Up";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(this.title),
+        ),
+        body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+              Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text("Mombie",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      fontFamily: 'Roboto')),
+            ),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: SignInButton(
+                  Buttons.Email,
+                  text: "Sign up with Email",
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EmailSignUp()),
+                    );
+                  },
+                )),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: SignInButton(
+                  Buttons.Google,
+                  text: "Sign up with Google",
+                  onPressed: () {
+                    signInWithGoogle().then((result) {
+                      if (result != null) {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return Home(uid: result.split(" ")[39].split(")")[0]);  // TODO: pass the uid correctly
+                            },
+                          ),
+                        );
+                      }
+                    });
+                  },
+                )),
+
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: GestureDetector(
+                    child: Text("Log In Using Email",
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue)),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => EmailLogIn()),
+                      );
+                    }))
+          ]),
+        ));
+  }
+}
+
+String name;
+String email;
+String imageUrl;
+
+Future<String> signInWithGoogle() async {
+  await Firebase.initializeApp();
+
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+
+  final UserCredential authResult = await _auth.signInWithCredential(credential);
+  final User user = authResult.user;
+
+  if (user != null) {
+    // Add the following lines after getting the user
+    // Checking if email and name is null
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(user.photoURL != null);
+
+    // Store the retrieved data
+    name = user.displayName;
+    email = user.email;
+    imageUrl = user.photoURL;
+
+    // Only taking the first part of the name, i.e., First Name
+    if (name.contains(" ")) {
+      name = name.substring(0, name.indexOf(" "));
+    }
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final User currentUser = _auth.currentUser;
+    assert(user.uid == currentUser.uid);
+
+    dbRef.child(user.uid).set({
+      "email": email,
+      "name": name
+    });
+
+    print('signInWithGoogle succeeded: $user');
+
+    return '$user';
+  }
+
+  return null;
+}
+
+Future<void> signOutGoogle() async {
+  await googleSignIn.signOut();
+
+  print("User Signed Out");
+}
